5eef7dea776650df35e8727c3f8c772881cffac5
cambalamas <cambalamas@gmail.com>
2021-09-09 21:48:34 +0200

    getting extensions and validation layers

--- include/Settings.h
+++ include/Settings.h
@@ -4,45 +4,41 @@
 
 namespace vo
 {
-
 // === EXTENSIONS
 //-----------------------------------------------------------------------------
 
 #define EXTENSION_SET 0
 
-#    if EXTENSION_SET==0
+#if EXTENSION_SET == 0
 inline constexpr std::array<const char *, 0> sExtensions {};
-#elif EXTENSION_SET==1
-//inline constexpr std::array sExtensions { "VK_KHR_portability_subset" };
-#elif EXTENSION_SET==2
-//inline constexpr std::array sExtensions { "VK_KHR_get_physical_device_properties2", "VK_KHR_portability_subset" };
+#elif EXTENSION_SET == 1
+// inline constexpr std::array sExtensions { "VK_KHR_portability_subset" };
+#elif EXTENSION_SET == 2
+// inline constexpr std::array sExtensions { "VK_KHR_get_physical_device_properties2", "VK_KHR_portability_subset" };
 #endif
-
 
 // === DEVICE EXTENSIONS
 //-----------------------------------------------------------------------------
 
 #define DEVICE_EXTENSION_SET 0
 
-#    if DEVICE_EXTENSION_SET==0
-[[maybe_unused]] inline constexpr std::array<std::string,0> sDeviceExtensions{};
-#elif DEVICE_EXTENSION_SET==1
+#if DEVICE_EXTENSION_SET == 0
+[[maybe_unused]] inline constexpr std::array<std::string, 0> sDeviceExtensions {};
+#elif DEVICE_EXTENSION_SET == 1
 [[maybe_unused]] inline constexpr std::array sDeviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
 #endif
-
 
 // === VALIDATION LAYERS
 //-----------------------------------------------------------------------------
 
 #define VALIDATION_SET 0
 
-#    if VALIDATION_SET==0
-inline constexpr std::array<const char *, 0> sLayers {};
-#elif VALIDATION_SET==1
-inline constexpr std::array sLayers { "VK_LAYER_KHRONOS_validation" };
+#if VALIDATION_SET == 0
+inline constexpr std::array<const char *, 0> sValidationLayers {};
+#elif VALIDATION_SET == 1
+inline constexpr std::array                  sValidationLayers { "VK_LAYER_KHRONOS_validation" };
 #endif
 
-inline constexpr bool sHasLayers { !sLayers.empty() };
+inline constexpr bool sHasValidationLayers { !sValidationLayers.empty() };
 
-
-}  // namespace vk0
+}  // namespace vo
--- src/Vonsai.cpp
+++ src/Vonsai.cpp
@@ -3,18 +3,33 @@
 #include <fmt/core.h>
 #include "Settings.h"
 
+//-----------------------------------------------------------------------------
+// === PRINTERS
+//-----------------------------------------------------------------------------
+
 #ifndef NDEBUG
 #  define VO_TRACE(lvl, msg) fmt::print("{} {}\n", std::string(lvl, '>'), msg)
 #else
 #  define VO_TRACE(s)
 #endif
 
-#define VO_CHECK(vulkanCode) \
-  if (vulkanCode != VK_SUCCESS) { \
-    fmt::print("[VK_ERR] - {}:{}\n↪ {}", __FILE__, __LINE__,#vulkanCode); \
-    exit(-1); \
-  }
+#define VO_ERR(msg)          fmt::print("[VO_ERR] - {}:{}\n↪ {}\n", __FILE__, __LINE__, msg)
+#define VO_ERR_FMT(msg, ...) VO_ERR(fmt::format(msg, __VA_ARGS__))
 
+#define VO_ERR_ABORT(msg) \
+  VOK_ERR(msg);           \
+  abort();
+
+#define VO_ERR_FMT_ABORT(msg) \
+  VOK_ERR_FMT(msg);           \
+  abort();
+
+#define VO_CHECK(vulkanCode) \
+  if (vulkanCode != VK_SUCCESS) { VO_ERR_ABORT(vulkanCode); }
+
+//===============================================
+//===============================================
+//===============================================
 
 //-----------------------------------------------------------------------------
 // === HELPERS
@@ -23,12 +38,11 @@
 std::vector<char const *> sGetRequiredExtensions()
 {
   uint32_t     glfwExtensionCount = 0;
-  char const ** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+  char const **glfwExtensions     = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
   std::vector<char const *> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
 
-  if (vo::sHasLayers)
-    extensions.emplace_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+  if (vo::sHasValidationLayers) extensions.emplace_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
 
   return extensions;
 }
@@ -37,33 +51,31 @@ std::vector<char const *> sGetRequiredExtensions()
 
 bool sCheckValidationLayerSupport()
 {
-#if 0
-  auto const availableLayers = vk::enumerateInstanceLayerProperties().value;
+  uint32_t layerCount;
+  vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+  std::vector<VkLayerProperties> availableLayers(layerCount);
+  vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
 
-  for (auto layerName : vk0::layers) {
-    auto const found = std::any_of(availableLayers.begin(), availableLayers.end(), [&](auto const layer) {
-      return std::string_view(layerName) == layer.layerName;
-    });
+  auto &L = availableLayers;
+  for (auto layerName : vo::sValidationLayers) {
+    auto const found =
+      std::any_of(L.begin(), L.end(), [&](auto &&l) { return std::string_view(layerName) == l.layerName; });
 
     if (!found) {
-      vk0AlertFmt("Layer {} not found.", layerName);
+      VO_ERR_FMT("Layer {} not found.", layerName);
       return false;
     }
   }
+
   return true;
-#else
-  return false;
-#endif
 }
 
 //===============================================
 //===============================================
 //===============================================
 
-
 namespace vo
 {
-
 //-----------------------------------------------------------------------------
 // === ENTRY POINT
 //-----------------------------------------------------------------------------
@@ -79,7 +91,6 @@ void Vonsai::run()
   VO_TRACE(1, "CLEAN UP");
   cleanup();
 }
-
 
 //-----------------------------------------------------------------------------
 // === TO RUN
@@ -102,16 +113,16 @@ void Vonsai::initVulkan()
 {
   VO_TRACE(2, "Create Instance");
   createInstance();
-//  VO_TRACE(2, "Setup Debug callback");
-//  mDebugMessenger.create(*mInstance);
-//  VO_TRACE(2, "Create Surface");
-//  createSurface();
-//  VO_TRACE(2, "Pick physical device");
-//  pickPhysicalDevice();
-//  VO_TRACE(2, "Create logical device");
-//  createLogicalDevice();
-//  VO_TRACE(2, "Create SWAP-CHAIN");
-//  createSwapChain();
+  //  VO_TRACE(2, "Setup Debug callback");
+  //  mDebugMessenger.create(*mInstance);
+  //  VO_TRACE(2, "Create Surface");
+  //  createSurface();
+  //  VO_TRACE(2, "Pick physical device");
+  //  pickPhysicalDevice();
+  //  VO_TRACE(2, "Create logical device");
+  //  createLogicalDevice();
+  //  VO_TRACE(2, "Create SWAP-CHAIN");
+  //  createSwapChain();
 }
 
 //-----------------------------------------------------------------------------
@@ -129,21 +140,20 @@ void Vonsai::mainLoop()
 void Vonsai::cleanup()
 {
   vkDestroyInstance(mVkInstance, nullptr);
-//  // NOTE: mInstance and mDevice destruction is handled by Unique..wrappers
-//
-//  // not using UniqeSwapchain to destroy in correct order - before the surface
-//  mLogicalDevice->destroySwapchainKHR(swapChain);
-//
-//  // NOTE: Surface is created by glfw, therefore not using a Unique handle
-//  mInstance->destroySurfaceKHR(mSurface);
-//
-//  // NOTE: Comment line below to verify that VALIDATION LAYERS are working...
-//  mDebugMessenger.destroy(*mInstance);
+  //  // NOTE: mInstance and mDevice destruction is handled by Unique..wrappers
+  //
+  //  // not using UniqeSwapchain to destroy in correct order - before the surface
+  //  mLogicalDevice->destroySwapchainKHR(swapChain);
+  //
+  //  // NOTE: Surface is created by glfw, therefore not using a Unique handle
+  //  mInstance->destroySurfaceKHR(mSurface);
+  //
+  //  // NOTE: Comment line below to verify that VALIDATION LAYERS are working...
+  //  mDebugMessenger.destroy(*mInstance);
 
   glfwDestroyWindow(mWindow);
   glfwTerminate();
 }
-
 
 //-----------------------------------------------------------------------------
 // === TO INIT VULKAN
@@ -151,83 +161,96 @@ void Vonsai::cleanup()
 
 void Vonsai::createInstance()
 {
-    VkApplicationInfo appInfo{};
-    appInfo.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName   = mTitle.c_str();
-    appInfo.applicationVersion = VK_API_VERSION_1_2;
-    appInfo.pEngineName        = (mTitle+" Engine").c_str();
-    appInfo.engineVersion      = VK_API_VERSION_1_2;
-    appInfo.apiVersion         = VK_API_VERSION_1_2;
+  VkApplicationInfo appInfo {};
+  appInfo.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo.pApplicationName   = mTitle.c_str();
+  appInfo.applicationVersion = VK_API_VERSION_1_2;
+  appInfo.pEngineName        = (mTitle + " Engine").c_str();
+  appInfo.engineVersion      = VK_API_VERSION_1_2;
+  appInfo.apiVersion         = VK_API_VERSION_1_2;
 
+  VkInstanceCreateInfo createInfo {};
+  createInfo.sType            = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo.pApplicationInfo = &appInfo;
 
-    VkInstanceCreateInfo createInfo{};
-    createInfo.sType            = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    createInfo.pApplicationInfo = &appInfo;
+  // . Esential extensions
+  uint32_t     glfwExtensionCount    = 0;
+  const char **glfwExtensions        = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+  createInfo.enabledExtensionCount   = glfwExtensionCount;
+  createInfo.ppEnabledExtensionNames = glfwExtensions;
 
-    // . Extensions
-    uint32_t glfwExtensionCount = 0;
-    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-    createInfo.enabledExtensionCount   = glfwExtensionCount;
-    createInfo.ppEnabledExtensionNames = glfwExtensions;
+  // . Show all available extensions
+  uint32_t extensionCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+  std::vector<VkExtensionProperties> extensions(extensionCount);
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+  // .. Show info about extensions
+  fmt::print("[*] Available DEVICE Extensions\n");
+  for (auto const &ext : extensions) { fmt::print(" ↳ {}\n", ext.extensionName); }
 
-    // . Layers
+  // . Layers
+  if (vo::sHasValidationLayers) {
+    createInfo.enabledLayerCount   = static_cast<uint32_t>(vo::sValidationLayers.size());
+    createInfo.ppEnabledLayerNames = vo::sValidationLayers.data();
+  } else {
     createInfo.enabledLayerCount = 0;
+  }
 
-    VO_CHECK(vkCreateInstance(&createInfo, nullptr, &mVkInstance));
+  VO_CHECK(vkCreateInstance(&createInfo, nullptr, &mVkInstance));
 }
 
 //-----------------------------------------------------------------------------
 
 void Vonsai::createSurface()
 {
-//  VkSurfaceKHR rawSurface;
-//  vk0TestFnC(glfwCreateWindowSurface(*mInstance, mWindow, nullptr, &rawSurface))
-//
-//  mSurface = rawSurface;
+  //  VkSurfaceKHR rawSurface;
+  //  vk0TestFnC(glfwCreateWindowSurface(*mInstance, mWindow, nullptr, &rawSurface))
+  //
+  //  mSurface = rawSurface;
 }
 
 //-----------------------------------------------------------------------------
 
 void Vonsai::pickPhysicalDevice()
 {
-//  auto const devices = vk0Call(mInstance->enumeratePhysicalDevices());
-//  if (devices.empty()) { vk0AlertAbort("Failed to find GPUs with Vulkan support!") }
-//
-//  for (const auto &device : devices) {
-//    auto const queueFamilyIndices = vk0::QueueFamilyIndices { device, mSurface };
-//
-//    if (queueFamilyIndices.isComplete()) {
-//      mQueueFamilyIndices = queueFamilyIndices;
-//      mPhysicalDevice     = device;
-//      break;
-//    }
-//  }
-//  if (!mPhysicalDevice) { vk0AlertAbort("Failed to find a suitable GPU") }
-//
-//#if vk0VERBOSE
-//  // . Getting extensions
-//  auto const extensions = vk0Call(mPhysicalDevice.enumerateDeviceExtensionProperties());
-//  // . Show info about extensions
-//  fmt::print("[*] Available DEVICE Extensions [{}]\n", fmt::ptr(&mPhysicalDevice));
-//  for (auto const &ext : extensions) { fmt::print(" ↳ {}\n", ext.extensionName); }
-//#endif
+  //  auto const devices = vk0Call(mInstance->enumeratePhysicalDevices());
+  //  if (devices.empty()) { vk0AlertAbort("Failed to find GPUs with Vulkan support!") }
+  //
+  //  for (const auto &device : devices) {
+  //    auto const queueFamilyIndices = vk0::QueueFamilyIndices { device, mSurface };
+  //
+  //    if (queueFamilyIndices.isComplete()) {
+  //      mQueueFamilyIndices = queueFamilyIndices;
+  //      mPhysicalDevice     = device;
+  //      break;
+  //    }
+  //  }
+  //  if (!mPhysicalDevice) { vk0AlertAbort("Failed to find a suitable GPU") }
+  //
+  //#if vk0VERBOSE
+  //  // . Getting extensions
+  //  auto const extensions = vk0Call(mPhysicalDevice.enumerateDeviceExtensionProperties());
+  //  // . Show info about extensions
+  //  fmt::print("[*] Available DEVICE Extensions [{}]\n", fmt::ptr(&mPhysicalDevice));
+  //  for (auto const &ext : extensions) { fmt::print(" ↳ {}\n", ext.extensionName); }
+  //#endif
 }
 
 //-----------------------------------------------------------------------------
 
 void Vonsai::createLogicalDevice()
 {
-//  auto const deviceFeatures   = vk::PhysicalDeviceFeatures();
-//  auto const queuesCreateInfo = mQueueFamilyIndices.getCreateInfo();
-//  auto const createInfo       = vk::DeviceCreateInfo { vk::DeviceCreateFlags(),
-//                                                 vk0SizeData(queuesCreateInfo),
-//                                                 vk0SizeData(vk0::layers),
-//                                                 vk0SizeData(vk0::extensions),
-//                                                 &deviceFeatures };
-//
-//  mLogicalDevice = vk0Call(mPhysicalDevice.createDeviceUnique(createInfo));
-//  mQueueGraphics = mLogicalDevice->getQueue(mQueueFamilyIndices.graphicsFamily.value(), 0);
-//  mQueuePresent  = mLogicalDevice->getQueue(mQueueFamilyIndices.presentFamily.value(), 0);
+  //  auto const deviceFeatures   = vk::PhysicalDeviceFeatures();
+  //  auto const queuesCreateInfo = mQueueFamilyIndices.getCreateInfo();
+  //  auto const createInfo       = vk::DeviceCreateInfo { vk::DeviceCreateFlags(),
+  //                                                 vk0SizeData(queuesCreateInfo),
+  //                                                 vk0SizeData(vk0::layers),
+  //                                                 vk0SizeData(vk0::extensions),
+  //                                                 &deviceFeatures };
+  //
+  //  mLogicalDevice = vk0Call(mPhysicalDevice.createDeviceUnique(createInfo));
+  //  mQueueGraphics = mLogicalDevice->getQueue(mQueueFamilyIndices.graphicsFamily.value(), 0);
+  //  mQueuePresent  = mLogicalDevice->getQueue(mQueueFamilyIndices.presentFamily.value(), 0);
 }
 
 //-----------------------------------------------------------------------------
@@ -236,4 +259,4 @@ void Vonsai::createSwapChain() {}
 
 //-----------------------------------------------------------------------------
 
-}  // namespace Vonsai
+}  // namespace vo
